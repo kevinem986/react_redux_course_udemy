0f2717d9bb768c990d37711c6c0e0876ed529733
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-09 05:52:21 +0200

    feat: adding inline CSS

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './custom.css'
 
 function App() {
   return (
-    <div className="outerdiv">
+    // <div className="outerdiv">
+    //   <div className="innerdiv">This is a green box</div>
+    // </div>
+    <div style={{padding:'70px 0', backgroundColor: 'blue'}}>
       <div className="innerdiv">This is a green box</div>
     </div>
   );
