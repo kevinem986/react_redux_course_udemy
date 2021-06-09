ab06c0d53191b2dcaca25f3e62a622ee049a1c0a
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-09 07:34:08 +0200

    feat: create project structure, and card component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import './custom.css'
+import './assets/css/custom.css';
+import Card from './components/Card';
 
 function App() {
 
-  const styles = {
-    outerdiv: {
-      padding: "70px 0",
-      backgroundColor: "blue"
-    },
-    innerdiv: {
-      textAlign: "center",
-      margin: "auto",
-      width: "60%",
-      border: "3px solid black",
-      backgroundColor: "#73AD21",
-      padding: "70px 0",
-    }
-  }
+  // const styles = {
+  //   outerdiv: {
+  //     padding: "70px 0",
+  //     backgroundColor: "blue"
+  //   },
+  //   innerdiv: {
+  //     textAlign: "center",
+  //     margin: "auto",
+  //     width: "60%",
+  //     border: "3px solid black",
+  //     backgroundColor: "#73AD21",
+  //     padding: "70px 0",
+  //   }
+  // }
 
-  const someText = 'Some Text';
-  const someDiv = <div style={{backgroundColor: 'white'}}>Some Div</div>
+  // const someText = 'Some Text';
+  // const someDiv = <div style={{backgroundColor: 'white'}}>Some Div</div>
 
   return (
     // <div className="outerdiv">
@@ -29,10 +30,15 @@ function App() {
     // </div>
     // <div style={styles.outerdiv}>
     // <div style={styles.innerdiv}>This is a green box</div>
-    <div style={styles.outerdiv}>
-    <div style={styles.innerdiv}>{someText}</div>
-    <div style={styles.innerdiv}>{someDiv}</div>
-  </div>
+    // <div style={styles.outerdiv}>
+    // <div style={styles.innerdiv}>{someText}</div>
+    // <div style={styles.innerdiv}>{someDiv}</div>
+  // </div>
+    <div className="App">
+      <Card />
+      <Card />
+      <Card />
+    </div>
   );
 }
 
