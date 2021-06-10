12f84e7287a61a2b5c958fd68b4dbe2b0aaef76d
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-10 04:15:03 +0200

    feat: adding rendering list of cards, create a new version of app.js and card component (index.js)

--- src/App.js
+++ src/App.js
@@ -1,82 +1,48 @@
 import React, { useState } from 'react';
 import './assets/css/custom.css';
 import Card from './components/Card';
-// import faker from 'faker';
+import faker from 'faker';
 
 function App() {
 
-  // const styles = {
-  //   outerdiv: {
-  //     padding: "70px 0",
-  //     backgroundColor: "blue"
-  //   },
-  //   innerdiv: {
-  //     textAlign: "center",
-  //     margin: "auto",
-  //     width: "60%",
-  //     border: "3px solid black",
-  //     backgroundColor: "#73AD21",
-  //     padding: "70px 0",
-  //   }
-  // }
-
-  // const someText = 'Some Text';
-  // const someDiv = <div style={{backgroundColor: 'white'}}>Some Div</div>
-
   //Hooks
-  const [name, setName] = useState('Alan Smith');
+  const [cards, setCards] = useState([
+    {
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      title: `${faker.name.title()}`,
+      avatar: `${faker.image.avatar()}`,
+    },
+    {
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      title: `${faker.name.title()}`,
+      avatar: `${faker.image.avatar()}`,
+    },
+    {
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      title: `${faker.name.title()}`,
+      avatar: `${faker.image.avatar()}`,
+    }
+  ]);
   const [showCard, setShowCard] = useState(true);
 
-  const changeNameHandler = name => setName(name);
-  const changeInputHandler = event => setName(event.target.value)
   const toggleShowCard = () => setShowCard(!showCard);
 
   // Elements
-  const buttonsMarkup = (
-    <div>
-      <button className="button button2">YES</button>
-      <button className="button button3">NO </button> 
-    </div>
-  );
-
   const cardsMarkup = (
-    showCard && 
-    <Card 
-      avatar="https://cdn.fakercloud.com/avatars/scottkclark_128.jpg" 
-      name={name} 
-      title="Human Markets Technician"
-      onChangeInput={changeInputHandler}
-      onChangeName={() => changeNameHandler('Michael Chan')}> 
-      {buttonsMarkup}
-    </Card>
+    showCard && (
+      cards.map(card => 
+        <Card 
+          avatar={card.avatar}
+          name={card.name} 
+          title={card.title} 
+        /> 
+      )      
+    )
   )
 
   return (
-    // <div className="outerdiv">
-    //   <div className="innerdiv">This is a green box</div>
-    // </div>
-    // <div style={{padding:'70px 0', backgroundColor: 'blue'}}>
-    //   <div className="innerdiv">This is a green box</div>
-    // </div>
-    // <div style={styles.outerdiv}>
-    // <div style={styles.innerdiv}>This is a green box</div>
-    // <div style={styles.outerdiv}>
-    // <div style={styles.innerdiv}>{someText}</div>
-    // <div style={styles.innerdiv}>{someDiv}</div>
-  // </div>
     <div className="App">
-      {/* <Card avatar={faker.image.avatar()} name={`${faker.name.firstName()} ${faker.name.lastName()}`} title={`${faker.name.title()}`}> 
-        {buttonsMarkup}
-      </Card>
-      <Card avatar={faker.image.avatar()} name={`${faker.name.firstName()} ${faker.name.lastName()}`} title={`${faker.name.title()}`}> 
-        {buttonsMarkup}
-      </Card>
-      <Card avatar={faker.image.avatar()} name={`${faker.name.firstName()} ${faker.name.lastName()}`} title={`${faker.name.title()}`}> 
-        {buttonsMarkup}
-      </Card> */}
-
        <button className="button" onClick={toggleShowCard}>Toggle Show/Hide</button>       
-       {/* <button className="button" onClick={() => changeNameHandler('John Doe')}>Change Name</button> */}
        {cardsMarkup}       
     </div>
   );
--- src/components/Card/index_V01.js
+++ src/components/Card/index_V01.js
@@ -2,13 +2,15 @@ import React from 'react'
 import '../../assets/css/custom.css';
 
 const Card = (props) => {
+    console.log(props);
     return (
         <div className="card">
             <img src={props.avatar} alt="Avatar" style={{width:"100%"}} />
             <div className="container">
                 <h4><b>{props.name}</b></h4> 
                 <p>{props.title}</p> 
-                <p><button className="button button-red">Delete</button></p>
+                <input type="text" value={props.name} onChange={props.onChangeInput} />
+                <p><button className="button" onClick={props.onChangeName}>Change Name</button></p>
                 <div>{props.children}</div> 
             </div>
         </div>
