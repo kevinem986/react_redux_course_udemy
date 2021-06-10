9871d166e13d518597192be39b37c7ed38708d3f
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-10 04:52:50 +0200

    feat: change the name of a specific card (two way binding)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
-import './assets/css/custom.css';
-import Card from './components/Card';
-import faker from 'faker';
+import React, { useState } from "react";
+import "./assets/css/custom.css";
+import Card from "./components/Card";
+import faker from "faker";
 
 function App() {
-
   //Hooks
   const [cards, setCards] = useState([
     {
-      id: 'card-01',
+      id: "card-01",
       name: `${faker.name.firstName()} ${faker.name.lastName()}`,
       title: `${faker.name.title()}`,
       avatar: `${faker.image.avatar()}`,
     },
     {
-      id: 'card-02',
+      id: "card-02",
       name: `${faker.name.firstName()} ${faker.name.lastName()}`,
       title: `${faker.name.title()}`,
       avatar: `${faker.image.avatar()}`,
     },
     {
-      id: 'card-03',
+      id: "card-03",
       name: `${faker.name.firstName()} ${faker.name.lastName()}`,
       title: `${faker.name.title()}`,
       avatar: `${faker.image.avatar()}`,
-    }
+    },
   ]);
   const [showCard, setShowCard] = useState(true);
 
@@ -32,29 +31,41 @@ function App() {
 
   const deleteCardHandler = (cardIndex) => {
     const cardsCopy = [...cards];
-    cardsCopy.splice(cardIndex,1);
+    cardsCopy.splice(cardIndex, 1);
     setCards(cardsCopy);
-  }
+  };
+
+  const changeNameHandler = (event, id) => {
+    // 1. Which card
+    const cardIndex = cards.findIndex((card) => card.id === id);
+    // 2. Make a copy of the cards
+    const cardsCopy = [...cards];
+    // 3. Change the name of the specific card
+    cardsCopy[cardIndex].name = event.target.value;
+    // 4. Set the cards with the latest version of card copy
+    setCards(cardsCopy);
+  };
 
   // Elements
-  const cardsMarkup = (
-    showCard && (
-      cards.map((card, index) => 
-        <Card 
-          key={card.id}
-          avatar={card.avatar}
-          name={card.name} 
-          title={card.title} 
-          onDelete={() => deleteCardHandler(index)}
-        /> 
-      )      
-    )
-  )
+  const cardsMarkup =
+    showCard &&
+    cards.map((card, index) => (
+      <Card
+        key={card.id}
+        avatar={card.avatar}
+        name={card.name}
+        title={card.title}
+        onDelete={() => deleteCardHandler(index)}
+        onChangeName={(event) => changeNameHandler(event, card.id)}
+      />
+    ));
 
   return (
     <div className="App">
-       <button className="button" onClick={toggleShowCard}>Toggle Show/Hide</button>       
-       {cardsMarkup}       
+      <button className="button" onClick={toggleShowCard}>
+        Toggle Show/Hide
+      </button>
+      {cardsMarkup}
     </div>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,6 +8,7 @@ const Card = (props) => {
             <div className="container">
                 <h4><b>{props.name}</b></h4> 
                 <p>{props.title}</p> 
+                <input type="text" value={props.name} onChange={props.onChangeName}></input>
                 <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
                 <div>{props.children}</div> 
             </div>
