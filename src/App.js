6b03a49072354e8b388f166f4202eef27a49e7db
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-17 02:50:08 +0200

    feat: convert functional base to class base (App.js)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Card from "./components/Elements/Card";
 import Button from "./components/Elements/Button";
 import "./assets/css/custom.css";
@@ -6,91 +6,92 @@ import faker from "faker";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
-  primary: '#4CAF50',
-  mango: 'yellow'
-}
+  primary: "#4CAF50",
+  mango: "yellow",
+};
 
-function App() {
-  //Hooks
-  const [cards, setCards] = useState([
-    {
-      id: "card-01",
-      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-      title: `${faker.name.title()}`,
-      avatar: `${faker.image.avatar()}`,
-    },
-    {
-      id: "card-02",
-      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-      title: `${faker.name.title()}`,
-      avatar: `${faker.image.avatar()}`,
-    },
-    {
-      id: "card-03",
-      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-      title: `${faker.name.title()}`,
-      avatar: `${faker.image.avatar()}`,
-    },
-  ]);
-  const [showCard, setShowCard] = useState(true);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  const toggleShowCard = () => setShowCard(!showCard);
+    this.state = {
+      cards: [
+        {
+          id: "card-01",
+          name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          title: `${faker.name.title()}`,
+          avatar: `${faker.image.avatar()}`,
+        },
+        {
+          id: "card-02",
+          name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          title: `${faker.name.title()}`,
+          avatar: `${faker.image.avatar()}`,
+        },
+        {
+          id: "card-03",
+          name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          title: `${faker.name.title()}`,
+          avatar: `${faker.image.avatar()}`,
+        },
+      ],
+      showCard: true,
+    };
+  }
 
-  const deleteCardHandler = (cardIndex) => {
-    const cardsCopy = [...cards];
+  toggleShowCard = () => this.setState({ showCard: !this.state.showCard });
+
+  deleteCardHandler = (cardIndex) => {
+    const cardsCopy = [...this.state.cards];
     cardsCopy.splice(cardIndex, 1);
-    setCards(cardsCopy);
+    this.setState({ cards: cardsCopy });
   };
 
-  const changeNameHandler = (event, id) => {
+  changeNameHandler = (event, id) => {
     // 1. Which card
-    const cardIndex = cards.findIndex((card) => card.id === id);
+    const cardIndex = this.state.cards.findIndex((card) => card.id === id);
     // 2. Make a copy of the cards
-    const cardsCopy = [...cards];
+    const cardsCopy = [...this.state.cards];
     // 3. Change the name of the specific card
     cardsCopy[cardIndex].name = event.target.value;
-    // 4. Set the cards with the latest version of card copy
-    setCards(cardsCopy);
+    // 4. Set the cards with the latest version of card copy    
+    this.setState({ cards: cardsCopy });
   };
 
-  // Dynamic button style
-  // const buttonStyle = {
-  //   backgroundColor: null
-  // }
+  render() {
+    const classes = ["button"];
 
-  // if(cards.length < 3) buttonStyle.backgroundColor = 'lightpink';
-  // if(cards.length < 2) buttonStyle.backgroundColor = 'red';
+    if (this.state.cards.length < 3) classes.push("pink");
+    if (this.state.cards.length < 2) classes.push("red text");
 
-  const classes = ['button'];
+    // Elements
+    const cardsMarkup =
+      this.state.showCard &&
+      this.state.cards.map((card, index) => (
+        <Card
+          key={card.id}
+          avatar={card.avatar}
+          name={card.name}
+          title={card.title}
+          onDelete={() => this.deleteCardHandler(index)}
+          onChangeName={(event) => this.changeNameHandler(event, card.id)}
+        />
+      ));
 
-  if(cards.length < 3) classes.push('pink');
-  if(cards.length < 2) classes.push('red text');
-
-  // Elements
-  const cardsMarkup =
-    showCard &&
-    cards.map((card, index) => (
-      <Card
-        key={card.id}
-        avatar={card.avatar}
-        name={card.name}
-        title={card.title}
-        onDelete={() => deleteCardHandler(index)}
-        onChangeName={(event) => changeNameHandler(event, card.id)}
-      />
-    ));
-
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Button color='mango' length={cards.length}>Toggle</Button>
-        <button className={classes.join(' ')} onClick={toggleShowCard}>
-          Toggle Show/Hide
-        </button>
-        {cardsMarkup}
-      </div>
-    </ThemeProvider>
-  );
+    return (
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Button color="mango" length={this.state.cards.length} onClick={this.toggleShowCard}>
+            Toggle
+          </Button>
+          <button className={classes.join(" ")} onClick={this.toggleShowCard}>
+            Toggle Show/Hide
+          </button>
+          {cardsMarkup}
+        </div>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
