6826a6bf7130ff3c7e10fd9bcfd436edb977e2b3
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-17 03:08:56 +0200

    feat: convert card component to class base component

--- src/components/Elements/Card/index.js
+++ src/components/Elements/Card/index.js
@@ -1,19 +1,31 @@
-import React from 'react'
-import '../../../assets/css/custom.css';
+import React, { Component } from "react";
+import "../../../assets/css/custom.css";
 
-const Card = (props) => {
+class Card extends Component {
+  render() {
     return (
-        <div className="card">
-            <img src={props.avatar} alt="Avatar" style={{width:"100%"}} />
-            <div className="container">
-                <h4><b>{props.name}</b></h4> 
-                <p>{props.title}</p> 
-                <input type="text" value={props.name} onChange={props.onChangeName}></input>
-                <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
-                <div>{props.children}</div> 
-            </div>
+      <div className="card">
+        <img src={this.props.avatar} alt="Avatar" style={{ width: "100%" }} />
+        <div className="container">
+          <h4>
+            <b>{this.props.name}</b>
+          </h4>
+          <p>{this.props.title}</p>
+          <input
+            type="text"
+            value={this.props.name}
+            onChange={this.props.onChangeName}
+          ></input>
+          <p>
+            <button className="button button-red" onClick={this.props.onDelete}>
+              Delete
+            </button>
+          </p>
+          <div>{this.props.children}</div>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default Card
+export default Card;
