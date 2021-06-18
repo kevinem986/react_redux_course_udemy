dde6309b0fd5e646fff85b80d76f86f5b22b24ee
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-18 06:37:50 +0200

    feat: adding multiple react context

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./assets/css/custom.css";
 import ComponentA from "./components/ComponentA";
 export const NameContext = React.createContext();
+export const ColorContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
       <NameContext.Provider value={"Smith"}>
-        <ComponentA />
+        <ColorContext.Provider value={"red"}>
+          <ComponentA />
+        </ColorContext.Provider>
       </NameContext.Provider>
     </div>
   );
