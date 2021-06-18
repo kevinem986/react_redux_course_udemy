dffd07f4379a3b9a6774879b471830b2257a92ff
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-18 06:26:55 +0200

    feat: ading react context (multiple components)

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Card from "./components/Elements/Card";
+import React from "react";
 import "./assets/css/custom.css";
-import { ThemeProvider } from "styled-components";
-import axios from "axios";
-
-const theme = {
-  primary: "#4CAF50",
-  mango: "yellow",
-};
+import ComponentA from "./components/ComponentA";
+export const NameContext = React.createContext();
 
 function App() {
-  //Hooks
-  const [card, setCard] = useState([]);
-  const [id, setId] = useState(1);
-
-  useEffect(() => {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        console.log(res.data);
-        setCard(res.data);
-      });
-  }, [id]);
-
-  const changeNameHandler = (event, id) => {
-    // 1. Make a copy of the cards
-    const cardCopy = { ...card };
-    // 2. Change the name of the specific card
-    cardCopy.name = event.target.value;
-    // 3. Set the cards with the latest version of card copy
-    setCard(cardCopy);
-  };
-
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
-        <Card
-          key={card.id}
-          name={card.name}
-          phone={card.phone}
-          onChangeName={(event) => changeNameHandler(event, card.id)}
-        />
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <NameContext.Provider value={"Smith"}>
+        <ComponentA />
+      </NameContext.Provider>
+    </div>
   );
 }
 
---
+++ src/components/ComponentC/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NameContext } from "../../App";
+
+const ComponentC = () => {
+  return (
+    <div>
+      <div>ComponentC</div>
+      <NameContext.Consumer>
+          {
+              name => {
+                  return <div>{name}</div>
+              }
+          }
+      </NameContext.Consumer>
+    </div>
+  );
+};
+
+export default ComponentC;
