487fb2333676966496b1bd2c1489cc5a4d69e9c8
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-06-30 02:53:12 +0200

    feat: adding reactstrap, bootstrap, and useReducer Hook (Simplified)

---
+++ src/components/Counter/index.js
@@ -0,0 +1,35 @@
+import React, { useReducer } from "react";
+import { Button, ButtonGroup } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const Counter = () => {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <div>Count: {count}</div>
+      <ButtonGroup>
+        <Button color="dark" onClick={() => dispatch('increment')}>Increment</Button>
+        <Button color="dark" onClick={() => dispatch('decrement')}>Decrement</Button>
+        <Button color="danger" onClick={() => dispatch('reset')}>Reset</Button>
+      </ButtonGroup>
+    </div>
+  );
+};
+
+export default Counter;
