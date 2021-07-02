d41fe385af860b6567a8199e9af436b49deff8d9
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-07-02 04:07:24 +0200

    feat: adding useReducer with useContext

---
+++ src/components/ComponentD/index.js
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import { Button, ButtonGroup } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { CounterContext } from "../../App";
+
+const ComponentD = () => {
+  const counterContext = useContext(CounterContext);
+  console.log(counterContext);
+  const { counter, dispatch } = counterContext;
+
+  return (
+    <div>
+      <ButtonGroup>
+        <Button color="primary" outline>
+          ComponentD Counter: {counter}
+        </Button>
+      </ButtonGroup>
+      <p></p>
+      <ButtonGroup>
+        <Button
+          color="dark"
+          onClick={() => dispatch({ type: "increment", payload: 1 })}
+        >
+          Increment
+        </Button>
+        <Button
+          color="dark"
+          onClick={() => dispatch({ type: "decrement", payload: 1 })}
+        >
+          Decrement
+        </Button>
+        <Button color="danger" onClick={() => dispatch({ type: "reset" })}>
+          Reset
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
+};
+
+export default ComponentD;
